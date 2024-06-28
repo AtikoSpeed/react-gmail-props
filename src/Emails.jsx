2bbd40f362b9808e846758cc6e6f63e7f7fa9d19
Atakan Temizkan <atikotemizkan@gmail.com>
2024-06-28 20:53:54 +0300

    done core criteria

---
+++ src/Emails.jsx
@@ -0,0 +1,16 @@
+import Email from "./Email.jsx";
+
+export default function Emails(props) {
+  return (
+    <ul>
+      {props.filteredEmails.map((email, index) => (
+        <Email
+          key={index}
+          email={email}
+          toggleStar={props.toggleStar}
+          toggleRead={props.toggleRead}
+        />
+      ))}
+    </ul>
+  );
+}
